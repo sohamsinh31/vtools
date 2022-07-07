c028b96c7c69b25872a996b526dd104935241747
sohamsinh31 <sohamb2019@gmail.com>
2022-07-07 11:15:47 +0530

    2.0

--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from http.client import HTTPResponse
+from typing import final
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from docx2pdf import convert
 from django.http import FileResponse
 from djangoconvertvdoctopdf.convertor import StreamingConvertedPdf
-import aspose.words as aw
+import PyPDF2
+import os
 from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
@@ -19,20 +21,22 @@ def upload(request):
     if request.method == 'POST':
         uploaded_file=request.FILES['document']
         name=uploaded_file.name
+        split_tup = os.path.splitext(name)
+        print(split_tup)
+        file_name = split_tup[0]
+        finalname = split_tup[0]+".pdf"
+        file_extension = split_tup[1]
         fs = FileSystemStorage()
         fs.save(name,uploaded_file)
         print(type(uploaded_file))
         print(type(uploaded_file.read()))
         print(type(str(uploaded_file.read())))
-        print("hi")
         pathh = "media/"+uploaded_file.name
-        fs.save("output.pdf",convert(pathh,"output.pdf"))
-        # doc = aw.Document(uploaded_file)
-        # doc.save("Output.pdf")
-        # for x in range(len(uploaded_file)):
-        #     convert(uploaded_file.read())
-        #     convert("{uploaded_file}}", "media\Mine.pdf")
-        #     # inst = StreamingConvertedPdf(uploaded_file)
-        #     # return inst.stream_content()
-        return open("media/output.pdf")
+        pathh2="media/"+finalname
+        convert(pathh,pathh2)
+        if convert:
+            print("success")
+        else:
+            print("kuchh or try karo bhai")
+        return FileResponse(open(pathh2, 'rb'), content_type='application/pdf')
     return render(request,'index.html')
