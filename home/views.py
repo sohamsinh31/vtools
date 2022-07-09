638390e229797ee96e12b7ce814c29aa0c52f5c5
sohamsinh31 <sohamb2019@gmail.com>
2022-07-09 13:26:45 +0530

    2.0

--- home/views.py
+++ home/views.py
@@ -7,7 +7,10 @@ from django.http import FileResponse
 from djangoconvertvdoctopdf.convertor import StreamingConvertedPdf
 import os
 from pytube import YouTube 
+from pathlib import Path
+import mimetypes
 from django.core.files.storage import FileSystemStorage
+from django import forms
 
 # Create your views here.
 def index(request):
@@ -36,11 +39,15 @@ def upload(request):
     return render(request,'index.html')
 
 def download(request):
-  if request.method == 'GET':
-    inp_value = request.GET.get('download', 'This is a default value')
-    print(inp_value)
-    url = YouTube(inp_value)
-    video = url.streams.get_highest_resolution()
-    video.download()
-    context = {'inp_value': inp_value}
-    return render(request,'download.html',context)
+    if request.method == 'POST':
+        inp_value = request.POST['download']
+        print(inp_value)
+        url = YouTube(inp_value)
+        video = url.streams.get_highest_resolution()
+        filename = video.default_filename
+        ffpath = video.get_file_path
+        response = HttpResponse(video, content_type='application/mp4')
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        return response
+        context = {'inp_value': inp_value}
+    return render(request,'download.html')
